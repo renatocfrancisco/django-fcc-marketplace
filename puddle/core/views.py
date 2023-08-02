179e596ca7f184deb0d4cd458701e4a5a33901e7
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-08-02 09:31:11 -0300

    feat: :sparkles: /signup

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from item.models import Category, Item
+
+from .forms import SignupForm
+
+from django.views.decorators.csrf import csrf_protect # because of the post method
 
 # Create your views here.
 
@@ -15,3 +19,18 @@ def index(req):
 
 def contact(req):
     return render(req, 'core/contact.html')
+
+@csrf_protect # because of the post method
+def signup(req):
+    if req.method == 'POST':
+        form = SignupForm(req.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    
+    return render(req, 'core/signup.html', {
+        'form': form
+    })
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -17,16 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# added by me
 from django.conf import settings
 from django.conf.urls.static import static
-# ---
-
-from core.views import index, contact
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # added by me

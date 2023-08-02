7914110e42df663a8a0a891a8fd13a40bfef5858
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-08-02 11:34:14 -0300

    feat: :sparkles: /items/new/

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Item
+from .forms import NewItemForm
 
 # Create your views here.
 def detail(req, pk):
@@ -10,4 +12,22 @@ def detail(req, pk):
     return render(req, 'item/detail.html', {
         'item': item,
         'related_items': related_items,
+    })
+    
+@login_required
+def new(req):
+    if req.method == 'POST':
+        form = NewItemForm(req.POST, req.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = req.user
+            item.save()
+            
+            return redirect('item:detail', pk=item.pk)
+    else:
+        form = NewItemForm()
+
+    return render(req, 'item/form.html', {
+        'form': form,
+        'title': 'New Item',
     })
